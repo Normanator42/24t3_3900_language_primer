a1770527c9de5519c750188800ca87a541060af2
Julian <z5359540@ad.unsw.edu.au>
2024-09-23 23:53:15 +1000

    done

--- server/server.py
+++ server/server.py
@@ -4,94 +4,65 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# Predefined data
+groups = [
+    {"id": 1, "groupName": "Group 1", "members": [1, 2, 3]},
+    {"id": 2, "groupName": "Group 2", "members": [4, 5]},
+]
+
+students = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+    {"id": 3, "name": "Charlie"},
+    {"id": 4, "name": "David"},
+    {"id": 5, "name": "Eve"},
+]
+
 @app.route('/api/groups', methods=['GET'])
 def get_groups():
-    """
-    Route to get all groups
-    return: Array of group objects
-    """
-    # TODO: (sample response below)
-    return jsonify([
-        {
-            "id": 1,
-            "groupName": "Group 1",
-            "members": [1, 2, 3],
-        },
-        {
-            "id": 2,
-            "groupName": "Group 2",
-            "members": [4, 5],
-        },
-    ])
+    return jsonify(groups)
 
 @app.route('/api/students', methods=['GET'])
 def get_students():
-    """
-    Route to get all students
-    return: Array of student objects
-    """
-    # TODO: (sample response below)
-    return jsonify([
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"},
-        {"id": 3, "name": "Charlie"},
-        {"id": 4, "name": "David"},
-        {"id": 5, "name": "Eve"},
-    ])
+    return jsonify(students)
 
 @app.route('/api/groups', methods=['POST'])
 def create_group():
-    """
-    Route to add a new group
-    param groupName: The name of the group (from request body)
-    param members: Array of member names (from request body)
-    return: The created group object
-    """
-    
-    # Getting the request body (DO NOT MODIFY)
     group_data = request.json
     group_name = group_data.get("groupName")
     group_members = group_data.get("members")
     
-    # TODO: implement storage of a new group and return their info (sample response below)
+    if not group_name or not group_members:
+        abort(400, "Group name and members are required")
 
-    return jsonify({
-        "id": 3,
-        "groupName": "New Group",
-        "members": [1, 2],
-    }), 201
+    new_group_id = max([group["id"] for group in groups]) + 1
+    new_group = {"id": new_group_id, "groupName": group_name, "members": group_members}
+    groups.append(new_group)
+
+    return jsonify(new_group), 201
 
 @app.route('/api/groups/<int:group_id>', methods=['DELETE'])
 def delete_group(group_id):
-    """
-    Route to delete a group by ID
-    param group_id: The ID of the group to delete
-    return: Empty response with status code 204
-    """
-    # TODO: (delete the group with the specified id)
-
-    return '', 204  # Return 204 (do not modify this line)
+    global groups
+    group = next((g for g in groups if g["id"] == group_id), None)
+    if group is None:
+        abort(404, "Group not found")
+    
+    groups = [g for g in groups if g["id"] != group_id]
+    return '', 204
 
 @app.route('/api/groups/<int:group_id>', methods=['GET'])
 def get_group(group_id):
-    """
-    Route to get a group by ID (for fetching group members)
-    param group_id: The ID of the group to retrieve
-    return: The group object with member details
-    """
-    # TODO: (sample response below)
-    return jsonify({
-        "id": 1,
-        "groupName": "Group 1",
-        "members": [
-            {"id": 1, "name": "Alice"},
-            {"id": 2, "name": "Bob"},
-            {"id": 3, "name": "Charlie"},
-        ],
-    })
-    # TODO:
-    # if group id isn't valid:
-    #     abort(404, "Group not found")
+    group = next((g for g in groups if g["id"] == group_id), None)
+    if group is None:
+        abort(404, "Group not found")
+    
+    group_details = {
+        "id": group["id"],
+        "groupName": group["groupName"],
+        "members": [student for student in students if student["id"] in group["members"]]
+    }
+    return jsonify(group_details)
 
 if __name__ == '__main__':
     app.run(port=3902, debug=True)
